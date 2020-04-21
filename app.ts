491412190eead92963e972c6725ff2fe3eb18984
oromiaGodanna <ori.gsero@gmail.com>
2020-04-21 22:58:12 +0300

    (feat) changed the app to apollo-express

--- app.ts
+++ app.ts
@@ -1,19 +1,29 @@
-import { ApolloServer } from 'apollo-server'
+import { ApolloServer } from 'apollo-server-express'
+import express from 'express'
 import { schemas } from './utils/schema'
 import users from './types/user/user.resolver'
 import accounts from './types/ecommerceaccount/ecommerceaccount.resolver'
 import cart from './types/cart/cart.resolver'
 import { merge } from 'lodash'
+import { BotService } from './utils/common';
+
+const app = express();
+//let bot = new BotService();
 
 ( async()=>{
-
+  BotService.startBot();
   const server = new ApolloServer({
     typeDefs: schemas,
     resolvers: merge({}, users,cart, accounts)
     
   })
 
-const {url} = await server.listen({port:8000})
-console.log(`server ready at ${url}`)
+  server.applyMiddleware({app})
+
+  app.listen({port:3000}, function(){
+    console.log(`Server ready at http://localhost:3000${server.graphqlPath}`)
+  });
+  
 
 })()
+
