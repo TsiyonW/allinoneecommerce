beccd6955cd64422a29b822a41dba3376ab6e4a2
oromiaGodanna <ori.gsero@gmail.com>
2020-04-26 10:51:20 +0300

    (feat)rearranged commands

--- utils/common.ts
+++ utils/common.ts
@@ -1,30 +1,33 @@
+import search from '../types/search/search.resolver'
 
 export let bot: any;
 
 export class BotService{
 
     static startBot() {
-        //let bot;
+      
         const TelegramBot = require('node-telegram-bot-api');
+
         //token given from telegram botfather
         const token = process.env.BOT_TOKEN;
-        console.log(token);
-        console.log(process.env.NODE_ENV)
+        console.log(token)
+
         if (process.env.NODE_ENV === 'production'){
             bot = new TelegramBot(token);
-            bot.setWebHook('https://my-web-root.com/' + bot.token);
+            bot.setWebHook('' + bot.token); //use webhooks in production
 
         }else{
-            console.log('Definitly pooling')
+            //otherwise use polling for faster experiance
             bot = new TelegramBot(token, {polling: true})
         }
+
+        //on start command
         bot.onText(/\/start/, function (message: any) {
+
             const chatId = message.chat.id;
-          
-            // send a message to the chat acknowledging receipt of their message
-            bot.sendMessage(
-                chatId, 
-                `👋Welcome, @${message.chat.username} to @all_in_one_ecommerce_bot. \nHere you can 🔎search, and control all your ecommerce accounts at one place. 
+            // send a message to the chat 
+            bot.sendMessage( chatId, 
+                `👋Welcome, @${message.chat.username} to @all_in_one_ecommerce_bot. \nHere you can 🔎search from multiple sites and save items for later use. 
                 \nFor a detailed info, please use /help!`,
                 {
                     'reply_markup': {
@@ -32,48 +35,39 @@ export class BotService{
                             [
                               {
                                 text: 'Search',
-                                // we shall check for this value when we listen
-                                // for "callback_query"
                                 callback_data: 'search'
                               },
                               {
-                                  text: 'Add e-commerce account',
-                                  callback_data: 'add_account'
+                                  text: 'Saved Items',
+                                  callback_data: 'saved_items'
                               }
                             ]
                           ]
                     }
                  } );
           });
-          bot.onText(/\/search/, function (message: any) {
-            const chatId = message.chat.id;
 
-            //call the search function
+          //on '/search' command
+          bot.onText(/\/search/, function (message: any, match: any) {
+            const chatId = message.chat.id;
+            const item = match[1]
+            console.log(item);
+            
           });
 
-          bot.onText(/\/add_/, function (message: any) {
+          //on '/saved_items' command
+          bot.onText(/\/saved_items/, function (message: any) {
             const chatId = message.chat.id;
+            //get saved items with chat id
           });
 
-          bot.onText(/\/my_cart/, function (message: any) {
-            const chatId = message.chat.id;
-          });
-
-          bot.onText(/\/my_wishlist/, function (message: any) {
-            const chatId = message.chat.id;
-          });
-
-          bot.onText(/\/orders/, function (message: any) {
-            const chatId = message.chat.id;
-          });
-
-          bot.onText(/\/settings/, function (message: any) {
-            const chatId = message.chat.id;
-          });
-
+          // on '/help' command
           bot.onText(/\/help/, function (message: any) {
             const chatId = message.chat.id;
+            //send some message
           });
+
+
           bot.on('callback_query', function onCallbackQuery(callbackQuery: any) {
             const action = callbackQuery.data;
             const msg = callbackQuery.message;
@@ -86,12 +80,13 @@ export class BotService{
             if (action === 'search') {
               text = 'search clicked';
             }
-            else if(action === 'add_account'){
+            else if(action === 'add_item'){
                 text = 'add ecommerce account clicked';
-            }
+            }else if(action === 'saved_items'){
+
+            }else if(action === "remove_item")
           
             bot.sendMessage(opts.chat_id, text);
           });
     }
 }
-
